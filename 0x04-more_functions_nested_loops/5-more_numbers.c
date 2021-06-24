954a40cefdc252a86579eed706319e8974585088
ruby-alx <rattari.ae@gmail.com>
2021-06-24 11:19:01 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,29 @@
 #include "holberton.h"
 
 /**
- * print_line - prints underscores
- * @n: limit
+ * more_numbers - prints numbers from 0 to 14 10 times
  *
  * Return: void
 */
 
-void print_line(int n)
+void more_numbers(void)
 {
-int i;
+int i, j, a;
 
-if (n > 0)
+for (a = 0; a < 10; a++)
 {
-for (i = 0; i < n; i++)
+for (i = 0; i <= 1; i++)
 {
-_putchar('_');
+for (j = 0; j <= 9; j++)
+{
+if (!(i == 1 && j >= 5))
+{
+if (i)
+_putchar(i + '0
+_putchar(j + '0'
+}
 }
 }
 _putchar('\n');
+}
 }
