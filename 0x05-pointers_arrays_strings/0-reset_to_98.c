c459a8a28cf9e4d13167ee6362760a68816e04b4
ruby-alx <rattari.ae@gmail.com>
2021-06-28 12:04:34 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - use a pointer to update the value of an integer
  * @n: parameter
- * Descriiption - reset_to_98 resets the value of *n to 98
+ * Description - reset_to_98 resets the value of *n to 98
  * Return: void
 */
 void reset_to_98(int *n)
