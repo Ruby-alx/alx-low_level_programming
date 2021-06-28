eec052f4f9facefed9d134e896782dabafcef229
ruby-alx <rattari.ae@gmail.com>
2021-06-28 12:47:43 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer - char
+ * Return: void
+*/
+void rev_string(char *s)
+{
+int i, c, l;
+char h;
+
+for (i = 0; s[i] != '\0'; i++)
+;
+
+l = i;
+for (i--, c = 0; c < l / 2; i--, c++)
+{
+h = s[c];
+s[c] = s[i];
+s[i] = h;
+}
+}
