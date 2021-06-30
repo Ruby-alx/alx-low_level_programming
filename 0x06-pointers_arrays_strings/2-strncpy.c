ac1c081d9c80dfd9d76dc5df4faa8cb127b17211
ruby-alx <rattari.ae@gmail.com>
2021-06-30 13:43:28 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - string copy
+ * @dest: first input character
+ * @src: second input character
+ * @n: number of bytes
+ * Return: destination
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+for ( ; i < n && src[i] != '\0'; ++i)
+{
+dest[i] = src[i];
+}
+while (i < n)
+{
+dest[i] = '\0';
+++i;
+}
+return (dest);
+}
