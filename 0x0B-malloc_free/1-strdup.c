e5858f72d8521d267374629d06d9215e63d05608
ruby-alx <rattari.ae@gmail.com>
2021-07-13 13:09:35 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strdup - duplicate a string in memory
+ * @str: the pointer to string
+ *
+ * Return: pointer to allocated space
+*/
+
+char *_strdup(char *str)
+{
+char *array;
+unsigned int counter, size;
+
+if (str == NULL)
+return (NULL);
+for (counter = 0; str[counter]; counter++)
+{
+size++;
+}
+counter = 0;
+array = malloc(sizeof(char) * (size + 1));
+if (array == NULL || size == 0)
+return (NULL);
+for (counter = 0; str[counter]; counter++)
+array[counter] = str[counter];
+array[size - 1] = '\0';
+return (array);
+}
