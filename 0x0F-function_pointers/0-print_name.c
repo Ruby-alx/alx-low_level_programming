81124d0449b969777424945f409429353e90d753
ruby-alx <rattari.ae@gmail.com>
2021-07-21 12:57:28 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: input params for name
- * @f: reps a function pointer
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
 */
-
 void print_name(char *name, void (*f)(char *))
 {
 if (name && f)
-{
 f(name);
-}
 }
