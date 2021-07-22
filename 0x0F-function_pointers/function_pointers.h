e0fcdfd6c31c71e6f2dfb8ee0649d1e4c5a7fee7
ruby-alx <rattari.ae@gmail.com>
2021-07-22 02:12:31 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,3 @@
-#ifndef _function_pointers_
-#define _function_pointers_
-#include <stdio.h>
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
